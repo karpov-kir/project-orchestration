853e8cfd6b2726ba9c32f0e0845c9db42e585340
Kirill Karpov <karpov.kir.v@gmail.com>
2024-02-17 18:04:40 +0400

    Set sites cache TTL to 60 secs

--- sites/k-k.pro/src/app/[id]/page.tsx
+++ sites/k-k.pro/src/app/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { NotionPage } from '@/components/NotionPage';
 import { notionApi } from '@/notionApi';
 
+export const revalidate = 60;
+
 export default async function Page({ params }: { params: { id: string } }) {
   const recordMap = await notionApi.getPage(params.id);
 
--- sites/k-k.pro/src/app/page.tsx
+++ sites/k-k.pro/src/app/page.tsx
@@ -2,6 +2,8 @@ import { NotionPage } from '@/components/NotionPage';
 import { config } from '@/config';
 import { notionApi } from '@/notionApi';
 
+export const revalidate = 60;
+
 export default async function Page() {
   const recordMap = await notionApi.getPage(config.rootNotionPageId);
 
