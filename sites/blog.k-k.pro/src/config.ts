8f2937b1d2b0cb75f847b988b7193095ca6aebbf
Kirill Karpov <karpov.kir.v@gmail.com>
2024-05-24 13:22:20 +0400

    Change title of blog-k-k

--- sites/blog.k-k.pro/src/config.ts
+++ sites/blog.k-k.pro/src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   rootNotionPageId: 'd35d65d727944aa79f68dc5a15f3b61f',
-  title: 'Blog of Kirill Karpov',
+  title: `Kirill Karpov's blog`,
   description: 'A software engineer blog',
   lang: 'en',
   domain: 'blog.k-k.pro',
