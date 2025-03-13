5cbeb22da396ec718d020d0b81dc9fbf6f34e907
Kirill Karpov <karpov.kir.v@gmail.com>
2025-03-13 20:09:22 +0400

    Refine 404 page for kk-forge.com

--- sites/kk-forge.com/src/routes/[...404].tsx
+++ sites/kk-forge.com/src/routes/[...404].tsx
@@ -1,12 +1,17 @@
 import { Title } from '@solidjs/meta';
 import { HttpStatusCode } from '@solidjs/start';
 
+import { SectionHeader } from '~/components/SectionHeader';
+
 export default function NotFoundRoute() {
   return (
     <>
-      <Title>KK Forge: Not Found</Title>
+      <Title>KK Forge: Not found</Title>
       <HttpStatusCode code={404} />
-      <h1 class="text-slate-100">Page Not Found</h1>
+      <SectionHeader>Oops! Page not found</SectionHeader>
+      <p class="text-slate-100 light:text-gray-800 transition-colors text-lg mb-5">
+        It looks like the page you're looking for has either been moved or doesn't exist.
+      </p>
     </>
   );
 }
